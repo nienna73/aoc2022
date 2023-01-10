4f3c0e865dbc06b31cc33985ecfb44e85f1626f1
jcpoulin <jcpoulin@ualberta.ca>
2023-01-09 18:13:10 -0700

    vis for day 8 poc

--- day13/solution.cpp
+++ day13/solution.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <iostream>
 
+// referencing : https://github.com/vss2sn/advent_of_code/blob/master/2022/cpp/day_13a.cpp
+
 vector<string> removeBlankLines(vector<string> input) {
     vector<string> ret;
     for (string s : input) {
@@ -14,215 +16,107 @@ vector<string> removeBlankLines(vector<string> input) {
     return ret;
 }
 
-vector<string> fixValues(vector<string> input) {
-    vector<string> ret;
-    for (string s : input) {
-        string tmp = "";
-        for (char c : s) {
-            if (isdigit(c)) {
-                tmp.append(1,'[');
-                tmp.append(1, c);
-                tmp.append(1, ']');
-            } else {
-                tmp += c;
+bool isNumber(char c) {
+    string s = "0123456789";
+    return s.find(c) != string::npos;
+}
+
+
+vector<tuple<int, bool> > getOrders(vector<string> values) {
+    vector<tuple<int, bool> > ret;
+    int j = 0;
+    int index = 1;
+    string part1 = "";
+    string part2 = "";
+    while (j < values.size()) {
+        if (values[j] != "" && part1 == "") {
+            part1 = values[j];
+            j++;
+            continue;
+        } else if (values[j] != "" && part2 == "") {
+            part2 = values[j];
+            j++;
+        } else if (values[j] == "") {
+            part1 = "";
+            part2 = "";
+            j++;
+            continue;
+        }
+        // cout << part1 << ", " << part2 << endl;
+        int i1 = 0;
+        int i2 = 0;
+        bool found = false;
+        while (i1 < part1.length() && i2 < part2.length() && !found) {
+            int n1 = 0;
+            while (isNumber(part1[i1])) {
+                n1 = n1 * 10 + (int) part1[i1];
+                i1++;
+            }
+            int n2 = 0;
+            while (isNumber(part2[i2])) {
+                n2 = n2*10 + (int) part2[i2];
+                i2++;
+            }
+
+            if (n1 == n2 && n1 != 0) {
+                // packets are equal so far
+                continue;
+            } else if (n1 < n2) {
+                // packets are in right order
+                tuple<int, bool> r = make_tuple(index, true);
+                ret.push_back(r);
+                found = true;
+            } else if (n1 > n2) {
+                // packets are in wrong order
+                tuple<int, bool> r = make_tuple(index, false);
+                ret.push_back(r);
+                found = true;
+            } else if (part1[i1] == part2[i2]) {
+                i1++;
+                i2++;
+            } else if (part1[i1] == ']') {
+                // packets are in right order
+                tuple<int, bool> r = make_tuple(index, true);
+                ret.push_back(r);
+                found = true;
+            } else if (part1[i1] == '[' || part1[i1] == ',') {
+                i1++;
+            } else if (part2[i2] == '[' || part2[i2] == ',') {
+                i2++;
             }
         }
-        ret.push_back(tmp);
+        if (i1 == part1.length() && !found) {
+            // packets are in right order
+            tuple<int, bool> r = make_tuple(index, true);
+            ret.push_back(r);
+        } else if (!found) {
+            tuple<int, bool> r = make_tuple(index, false);
+            ret.push_back(r);
+            i1 = part1.length();
+        }
+        index++;
     }
     return ret;
-}
-
-int findEndIndex(string part, int i) {
-    int ret = i;
-    for (int j = i; j < part.length(); j++) {
-        if (part[j] == ']') {
-            return j;
-        }
-    }
-    return ret;
-}
-
-string getStringChunk(string s, int start, int end) {
-    string ret = "";
-    for (int i = start; i <= end; i++) {
-        ret += s[i];
-    }
-    return ret;
-}
-
-int countChar(string s, char c) {
-    int count = 0;
-    for (char a : s) {
-        if (a == c) {
-            count += 1;
-        }
-    }
-    return count;
-}
-
-bool containsDigit(string s) {
-    for (int i = 0; i < s.length(); i++) {
-        if (isdigit(s[i])) {
-            return true;
-        }
-    }
-    return false;
-}
-
-int getDigit(string c) {
-    string ret = "";
-    for (int i = 0; i < c.length(); i++) {
-        if (isdigit(c[i])) {
-            ret += c[i];
-        }
-    }
-    return stoi(ret);
-}
-
-string findChunk(string part, int j) {
-    int openBracketsNeeded = 0;
-    int openBracketsFound = 0;
-    int closeBracketsFound = 0;
-    string ret = "";
-    for (char c : part) {
-        if (c == '[' && openBracketsNeeded < j) {
-            openBracketsNeeded += 1;
-        }
-        else if (openBracketsNeeded >= j) {
-            if (c == '[') {
-                openBracketsFound += 1;
-                ret += c;
-            } else if (c == ']') {
-                closeBracketsFound += 1;
-                ret += c;
-            } else {
-                ret += c;
-            } 
-            if (openBracketsFound == closeBracketsFound) {
-                return ret;
-            }
-        }
-    }
-    return ret;
-}
-
-int getFirstNumber(string chunk) {
-    int ret;
-    for (int i = 0; i < chunk.length(); i++) {
-        char c = chunk[i];
-        if (i+1 < chunk.length()) {
-            if (isdigit(c) && isdigit(chunk[i+1])) {
-                string number = "";
-                number.append(&c);
-                number.append(&chunk[i+1]);
-                ret = stoi(number);
-            }
-        } else if (isdigit(c)) {
-            ret = (int) c;
-        }
-    }
-    return ret;
-}
-
-bool firstNumberBigger(string chunk1, string chunk2) {
-    int i1 = getFirstNumber(chunk1);
-    int i2 = getFirstNumber(chunk2);
-    return i1 > i2;
     
 }
 
-vector<tuple<int, bool> > getCorrectPairs(vector<string> input) {
-    vector<tuple<int, bool> > ret;
-    for (int i = 0; i < input.size(); i += 2) {
-        cout << "Pair " << i/2 << endl;
-        string part1 = input[i];
-        string part2 = input[i+1];
-        vector<string> part1Split = splitString(part1, ',');
-        vector<string> part2Split = splitString(part2, ',');
-        int j = 1;
-        int k = 1;
-        int numOpen1 = 0;
-        int numClose1 = 0;
-        int numOpen2 = 0;
-        int numClose2 = 0;
-        bool insideList;
-        tuple<int, bool> pair = make_tuple(-1, false);
-        while (j < part1.length() && k < part2.length()) {
-            char c1 = part1[j];
-            char c2 = part2[k];
-            if (j >= part2.length()) {
-                // right side ran out of items
-                pair = make_tuple(1/2, false);
-                j = part1.length();
-            }
-            else if (isdigit(c1) && isdigit(c2)) {
-                if ((int) c1 > (int) c2) {
-                    pair = make_tuple(i/2, false);
-                    j = part1.length();
-                } else if ((int) c1 < (int) c2) {
-                    pair = make_tuple(i/2, true);
-                    j = part1.length();
-                }
-            } else if (c1 != ']' && c2 != ']') {
-                while (!isdigit(c1)) {
-                    c1 = part1[j];
-                    j++;
-                }
-                while (!isdigit(c2)) {
-                    c2 = part2[k];
-                    k++;
-                }
-                if ((int) c1 > (int) c2) {
-                    pair = make_tuple(i/2, false);
-                    j = part1.length();
-                } else if ((int) c1 < (int) c2) {
-                    pair = make_tuple(i/2, true);
-                    j = part1.length();
-                }
-            } else if (isdigit(c1) && c2 == '[') {
-                pair = make_tuple(i/2, true);
-                j = part1.length();
-            } else {
-                if (isdigit(c1) && c2 == ']') {
-                    pair = make_tuple(i/2, false);
-                    j = part1.length();
-                } else if (isdigit(c2) && c1 == ']') {
-                    pair = make_tuple(i/2, true);
-                    j = part1.length();
-                }
-            }
-            j++;
-            k++;
+int getSumPairsInRightOrder(vector<tuple<int, bool> > inOrder) {
+    int ret = 0;
+    for (tuple<int, bool> t : inOrder) {
+        if (get<1>(t)) {
+            cout << "Pair " << get<0>(t) << " is in right order" << endl;
+            ret += get<0>(t);
         }
-        if (get<0>(pair) == -1) {
-            if (part1.size() > part2.size()) {
-                pair = make_tuple(i/2, false);
-            } else {
-                pair = make_tuple(i/2, true);
-            }
-        }
-        cout << get<1>(pair) << endl;
-        ret.push_back(pair);
     }
     return ret;
 }
 
-int getSumPairsInRightOrder(vector<tuple<int, bool> > pairs) {
-    int ret = 0;
-    for (tuple<int, bool> t : pairs) {
-        if (get<1>(t)) {
-            cout << "Index: " << get<0>(t) << endl;
-            ret += get<0>(t) + 1;
-        }
-    }
-    return ret;
-}
 
 int main() {
     vector<string> values = getValuesFromFile("input.txt");
-    values = removeBlankLines(values);
-    vector<tuple<int, bool> > correctIndexes = getCorrectPairs(values);
-    int sum = getSumPairsInRightOrder(correctIndexes);
+    // values = removeBlankLines(values);
+    vector<tuple<int, bool> > inOrder = getOrders(values);
+    int sum = getSumPairsInRightOrder(inOrder);
     cout << "Sum: " << sum << endl;
     return 0;
     // Part 1:
@@ -234,4 +128,5 @@ int main() {
     // 6388 is wrong
     // 5892 is wrong
     // 5825 is wrong
+    // 5552 is wrong
 }
